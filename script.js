c9bd27211041c47753a2f7e97d2beb08455e75a9
Manvesh Karande <130562971+Manveshkarande2000@users.noreply.github.com>
2023-05-15 00:47:58 +0530

    LEVEL 1 TASK 3

--- script.js
+++ script.js
@@ -1,54 +1,26 @@
-'use strict';
+const calculateTemp = () => {
+    const inputTemp = document.getElementById('temp').value;
 
+    const tempSelected = document.getElementById('temp_diff');
+    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
 
-
-/**
- * add event on element
- */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
+    // Celsius to Fahrenheit
+    const celToFah = (cel) => {
+        let fahrenheit = ((cel * 9 / 5) + 32).toFixed(1);
+        return fahrenheit;
     }
-  } else {
-    elem.addEventListener(type, callback);
-  }
+
+    // Fahrenheit to Celsius
+    const fahToCel = (fah) => {
+        let celsius = ((fah - 32) * 5 / 9).toFixed(1);
+        return celsius;
+    }
+
+    if (valueTemp == 'cel') {
+        document.getElementById("result").innerHTML = celToFah(inputTemp) + "&#176; Fahrenheit";
+    }
+    else {
+        document.getElementById("result").innerHTML = fahToCel(inputTemp) + "&#176; Celsius";
+    }
 }
 
-
-
-/**
- * navbar toggle
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
-
-const toggleNavbar = function () { navbar.classList.toggle("active"); }
-
-addEventOnElem(navTogglers, "click", toggleNavbar);
-
-const closeNavbar = function () { navbar.classList.remove("active"); }
-
-addEventOnElem(navLinks, "click", closeNavbar);
-
-
-
-/**
- * header & back top btn active
- */
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
